853bb74d5df273ee798e725389afb1f3d900b2a7
Mayowa Adeniyi <mayowaadeniyi@hotmail.com>
2022-03-28 01:10:46 +0100

    Beginning of three js work

---
+++ starter-v2/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    devServer: {
+        static: '.'
+    }
+}
